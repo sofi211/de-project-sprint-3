6dc699523eba570b0188bdebf0808d01a1964f1f
Asanina Sofia <130637111+sofi211@users.noreply.github.com>
2024-05-18 15:41:20 +0300

    Update mart.f_customer_retention_2.sql

--- src/mart.f_customer_retention_2.sql
+++ src/mart.f_customer_retention_2.sql
@@ -1,28 +1,28 @@
 delete from mart.f_customer_retention where period_id = (select first_day_of_week
-														 from mart.d_calendar
-														 where date_actual='{{ds}}'::date);
+							 from mart.d_calendar
+							 where date_actual='{{ds}}'::date);
 
 with tabl_status as (select first_day_of_week AS period_id,
-				            item_id,
-				            customer_id,
-			                payment_amount,
-				            quantity,
-				            case 
-					            when (count(*) over (partition by date_id, item_id, customer_id)=1) and (status!='refunded') then 'new' else status
-				            end status
-				      from mart.f_sales
-				      where first_day_of_week = (select first_day_of_week
-					  							 from mart.d_calendar
-												 where date_actual='{{ds}}'::date))
+			    item_id,
+			    customer_id,
+			    payment_amount,
+			    quantity,
+			    case 
+			        when (count(*) over (partition by date_id, item_id, customer_id)=1) and (status!='refunded') then 'new' else status
+			    end status
+		     from mart.f_sales
+		     where first_day_of_week = (select first_day_of_week
+					  	from mart.d_calendar
+						where date_actual='{{ds}}'::date))
 
 insert into mart.f_customer_retention (period_id,
-									   item_id,
-									   new_customers_revenue,
-									   returning_customers_revenue,
-									   customers_refunded,
-									   new_customers_count,
-									   returning_customers_count,
-									   refunded_customer_count)
+				       item_id,
+				       new_customers_revenue,
+				       returning_customers_revenue,
+				       customers_refunded,
+				       new_customers_count,
+				       returning_customers_count,
+				       refunded_customer_count)
 select period_id,
        item_id,
        sum(case when status = 'new' then payment_amount end) new_customers_revenue,
